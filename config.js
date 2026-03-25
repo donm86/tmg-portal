86d0b358a3261f2ad97872e25019165474c5fad9
donm86 <don@themccooeyegroup.ca>
2026-03-24 21:37:09 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// TMG Portal - API Configuration
+// This file is loaded before the app and sets the API key
+window.TMG_KEY = "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";
